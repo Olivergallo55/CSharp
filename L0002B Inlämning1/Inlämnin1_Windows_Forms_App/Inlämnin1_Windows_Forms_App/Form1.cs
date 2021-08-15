f0348a878d6b4069c556fa41f7954981df5a7321
Olivergallo55 <62659746+Olivergallo55@users.noreply.github.com>
2021-08-15 10:45:56 +0200

    Add files via upload

---
+++ L0002B Inlämning1/Inlämnin1_Windows_Forms_App/Inlämnin1_Windows_Forms_App/Form1.cs
@@ -0,0 +1,110 @@
+﻿/*
+Oliver Gallo 000504-2916
+oligal-1@student.ltu.se
+Kurs: L0002B
+*/
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Inlämnin1_Windows_Forms_App
+{
+    public partial class Form1 : Form
+    {
+        //Startar programmet
+        public Form1()
+        {
+            InitializeComponent();
+            richTextBox1.ReadOnly = true;
+            richTextBox1.BackColor = Color.White;
+        }
+
+        //Buttonclikck metoden när man trycker på knappen
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //tar ut texterna och gör om till intar
+                int first = int.Parse(textBox1.Text);
+                int second = int.Parse(textBox2.Text);
+
+                int result = second - first; //räknar ut resultaten
+
+                if (result > 0)//om resultaten är större än 0 räkna och skriv ut resultatet för användaren 
+                {
+                    richTextBox1.Text = "Det här blev resultaten:\n\n";
+                    CalculateResult(result);
+                }
+                else//annars skriv ut felmeddelande
+                {
+                    richTextBox1.Text = "Du kan inte betala mindre än vad det kostar\n";
+                }
+            }
+            catch (Exception)//skulle boxarna lämnas tomma eller skrivas med text skriv ut felmeddelande
+            {
+                richTextBox1.Text = "Du kan inte Ge text för nummer eller lämna den tomt\n";
+            }
+        }
+
+        //räknar ut resultaten och skriver ut det på skärmen
+        private void CalculateResult(int price)
+        {
+            int femhundra = price / 500;
+            price %= 500;
+            if (femhundra > 0)
+                richTextBox1.AppendText("Antal femhundra lappar: " + femhundra + '\n'+ '\n');
+
+            int hundra = price / 100;
+            price %= 100;
+            if (hundra > 0)
+                richTextBox1.AppendText("Antal hundra lappar: " + hundra + '\n' + '\n');
+
+
+            int femtiolapp = price / 50;
+            price %= 50;
+            if (femtiolapp > 0)
+                richTextBox1.AppendText("Antal femtio lappar: " + femtiolapp + '\n' + '\n');
+
+
+            int tjugolapp = price / 20;
+            price %= 20;
+            if (tjugolapp > 0)
+                richTextBox1.AppendText("Antal tjugo lappar: " + tjugolapp + '\n' + '\n');
+
+            int tio = price / 10;
+            price %= 10;
+            if (tio > 0)
+                richTextBox1.AppendText("Antal tio kroner: " + tio + '\n' + '\n');
+
+            int fem = price / 5;
+            price %= 5;
+            if (fem > 0)
+                richTextBox1.AppendText("Antal fem kroner: " + fem + '\n' + '\n');
+
+            int ett = price / 1;
+            price %= 1;
+            if (ett > 0)
+                richTextBox1.AppendText("Antal ett kroner: " + ett + '\n' + '\n');
+        }
+
+
+        /*
+         
+         Resten av koden här används inte men behövs för komponenterna
+         
+         */
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ L0002B Inlämning1/Inlämning1/Program.cs
@@ -0,0 +1,96 @@
+﻿/*
+Oliver Gallo 000504-2916
+oligal-1@student.ltu.se
+Kurs: L0002B
+*/
+
+using System;
+
+namespace Inlämning1
+{
+    class Program
+    {
+        //Main method
+        static void Main(string[] args)
+        {
+            Program pg = new Program(); //create new object of class
+
+            //try of the input/output part of the program, if the input did not have a nummeric value write error and starts the try all over
+            //same with if the payed value was less then the original pricing
+        First:
+            try
+            {
+                Console.WriteLine("Ange pris: ");
+                int price = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Betalat: ");
+                int payed = Convert.ToInt32(Console.ReadLine());
+
+                int result = payed - price;
+
+                if(result > 0)
+                Console.WriteLine("Totala tillbaka summan blev: " + result + "\n");
+
+                pg.CountPrice(result);
+
+                if (result < 0)
+                { 
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Du kan inte betala mindre än vad det kostar\n");
+                    Console.ResetColor();
+                    goto First;
+                }
+              
+            }
+            catch (FormatException)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("The given input have to be nummeric! \n");
+                Console.ResetColor();
+                goto First;
+            }
+        }
+
+        //Method to count out the change back of the price. It counts with 500, 100, 50, 20 and coins 10, 5, 1
+        //It only prints out the ones that are acctually in use
+        void CountPrice(int pris)
+        {
+           int femhundra = pris / 500;
+            pris %= 500;
+            if(femhundra > 0)
+                Console.WriteLine("Antal femhundra lappar: " + femhundra);
+
+          int hundra = pris / 100; 
+            pris %= 100;
+            if (hundra > 0)
+                Console.WriteLine("Antal hundra lappar: " + hundra);
+
+
+          int femtiolapp = pris / 50;
+            pris %= 50;
+            if (femtiolapp > 0)
+                Console.WriteLine("Antal femtio lappar: " + femtiolapp);
+
+
+            int tjugolapp = pris / 20;
+            pris %= 20;
+            if (tjugolapp > 0)
+                Console.WriteLine("Antal tjugo lappar: " + tjugolapp);
+
+            int tio = pris / 10;
+            pris %= 10;
+            if (tio > 0)
+                Console.WriteLine("Antal tio kroner: " + tio);
+
+            int fem = pris / 5;
+            pris %= 5;
+            if (fem > 0)
+                Console.WriteLine("Antal fem kroner: " + fem);
+
+            int ett = pris / 1;
+            pris %= 1;
+            if (ett > 0)
+                Console.WriteLine("Antal ett kroner: " + ett);
+        }
+    }
+}
