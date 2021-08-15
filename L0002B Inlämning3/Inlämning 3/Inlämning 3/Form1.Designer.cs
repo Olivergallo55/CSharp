062ac4d39126c55f6c20448d77118136127618f6
Olivergallo55 <62659746+Olivergallo55@users.noreply.github.com>
2021-08-15 10:46:34 +0200

    Add files via upload

---
+++ L0002B Inlämning3/Inlämning 3/Inlämning 3/Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿
+namespace Inlämning_3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button2 = new System.Windows.Forms.Button();
+            this.Förnamn = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.button2.Location = new System.Drawing.Point(432, 170);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(166, 57);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Rensa";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Förnamn
+            // 
+            this.Förnamn.AutoSize = true;
+            this.Förnamn.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Förnamn.Location = new System.Drawing.Point(51, 62);
+            this.Förnamn.Name = "Förnamn";
+            this.Förnamn.Size = new System.Drawing.Size(80, 20);
+            this.Förnamn.TabIndex = 6;
+            this.Förnamn.Text = "Förnamn";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(40, 138);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(93, 20);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Efternamn";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(7, 207);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(129, 20);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Personnummer";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button1.Location = new System.Drawing.Point(432, 62);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(166, 57);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "OK";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(654, 559);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Förnamn);
+            this.Controls.Add(this.button2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label Förnamn;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ L0002B Inlämning3/Inlämning 3/Inlämning 3/Form1.cs
@@ -0,0 +1,195 @@
+﻿/*
+Oliver Gallo 000504-2916
+oligal-1@student.ltu.se
+Kurs: L0002B
+*/
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Inlämning_3
+{
+    public partial class Form1 : Form
+    {
+        private RichTextBox n;
+        private TextBox firstName, lastName, prNummer; 
+
+        public Form1() 
+        {
+            InitializeComponent();
+            CreateGUI();
+        }
+
+        //Create the GUI of the interface
+        private void CreateGUI()
+        {
+            CreateText1();
+            CreateText2();
+            CreateText3();
+            CreateRichTextBox();
+        }     
+
+        //Creates the first textbox and adds it to the contol
+        private void CreateText1()
+        {
+            firstName = new TextBox
+            {
+                Location = new Point(98, 40),
+                Width = 145,
+                Height = 26,
+                BackColor = Color.White
+            };
+            Controls.Add(firstName);
+        }
+
+        //Creates the second textbox and adds it to the contol
+        private void CreateText2()
+        {
+            lastName = new TextBox
+            {
+                Location = new Point(98, 85),
+                Width = 145,
+                Height = 26,
+                BackColor = Color.White
+            };
+            Controls.Add(lastName);
+        }
+
+        //Creates the third textbox and adds it to the contol
+        private void CreateText3()
+        {
+            prNummer = new TextBox
+            {
+                Location = new Point(98, 130),
+                Width = 145,
+                Height = 26,
+                BackColor = Color.White
+            };
+            Controls.Add(prNummer);
+        }
+
+        //Create the richTextbox where the result will be printed
+        public void CreateRichTextBox()
+        {
+            n = new RichTextBox
+            {
+                ReadOnly = true,
+                Width = 250,
+                Height = 150,
+                Location = new Point(50, 180),
+                BackColor = Color.White
+            };
+            Controls.Add(n);
+        }
+      
+        //Functions for the button to do when its clicked
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            string firstname = "";
+            string lastname = "";
+            string gender = "";
+            long pr = 0;
+            Person pers = new Person();
+
+                n.ForeColor = Color.Red; //sets the printed text to red
+            if (!String.IsNullOrEmpty(firstName.Text) && !String.IsNullOrEmpty(lastName.Text) && !String.IsNullOrEmpty(prNummer.Text)) //checks if either one of the boxes are empty if they are it gives error message
+            {
+                firstname = firstName.Text;
+                lastname = lastName.Text;
+
+                //try to read the peronnummer and vontroll gender if the number is incorrect and button will be inaccesable, if not a number was input then start over by clearing everything
+                try
+                {
+                    pr = long.Parse(prNummer.Text);
+                    gender = pers.ControllGender(pr);
+                    if (pers.ControlPrnr(prNummer.Text) && prNummer.Text.Length == 10)
+                    {
+                        n.AppendText("Korrekt Personnummer\n\n");
+                        Person p = new Person(firstname, lastname, prNummer.Text, gender);
+                        n.AppendText(p.ToString()); //print out the object using its ToString method
+                    }else
+                        n.Text = "Inte Korrekt personnummer";
+                }
+                catch (Exception)
+                {
+                    n.Text = "Personnumret måste ha ett nummeriskt värde!\nRensa och försök igen";
+                    button1.Enabled = false;
+                }
+            }
+            else
+            {
+                n.Text = "Lämna inte nån av textBoxarna tomma!\nRensa och gör igen";
+                button1.Enabled = false;
+            }
+            button1.Enabled = false;
+        }
+
+        //Button action, clears everything and enables the OK button again so that it works
+        private void button2_Click(object sender, EventArgs e)
+        {
+            firstName.Text = "";
+            lastName.Text = "";
+            prNummer.Text = "";
+            n.Text = "";
+            button1.Enabled = true;
+        }             
+    } 
+}
+
+class Person
+{
+    string firstname, lastname, gender;
+    string personnummer;
+
+    public Person() { }
+
+    public Person(string firstname, string lastname, string personnummer, string gender)
+    {
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.personnummer = personnummer;
+        this.gender = gender;
+    }
+
+    //Method to controll the gender, read in the input as an integer value and divide 
+    public string ControllGender(long prNr)
+    {
+        /*
+        if (prNr / 10 % 2 == 0)    //divide prNr by 10 to take away the last number and modulo 2 to see if its even    
+            gender = "KVINNA";     //if even the gender is woman     
+        else           
+            gender = "MAN"; //and if its uneven the gender is man
+        */
+        return prNr / 10 % 2 == 0 ? "Kvinna" : "Man"; //return the gender on the screen
+    }
+
+    //Uses Lunhs algorithm to see if the personalnumber is correct or not
+    public bool ControlPrnr(String prNr)
+    {
+        int sum = 0;
+        bool isSecondNumber = false; //check if its a second number or not
+
+        for (int i = prNr.Length - 1; i >= 0; i--) //goes through the personalnumber from the end
+        {
+
+            int current = prNr[i] - '0'; // to get the nummeric value at the current number of the string
+
+            if (isSecondNumber == true)//if its a second number
+                current *= 2;//then multiply the current numbers value
+
+            sum += current / 10;  //divide the number with 10 (used for double digits, where the value is over 10, otherwise the value will be 0)
+            sum += current % 10;  //used for the simple digits, if the vale us is 15, it will add 5 to the sum or if the current value is 5 it will add 5 to the sum
+
+            isSecondNumber = !isSecondNumber;//if its a secondNumber then make it a fist number
+        }
+        return (sum % 10 == 0); //returns if the end number is equaly dividable                        
+    }
+
+
+    //method to print out the person corecctly
+    public override string ToString()
+    {         
+        return "Förnamn: " + firstname  + "\n\n" + "Efternamn: " + lastname + "\n\n" + "Personnummer: " + personnummer + "\n\n" + "Kön: " + gender;
+    }
+
+}
